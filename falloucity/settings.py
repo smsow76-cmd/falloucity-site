dad5da31d39de4d1c246a4de88807de6ae7b24be
talibe mame cheikh <sowserignemansour55@gmail.com>
2026-04-05 09:32:39 +0000

    sitemap

--- falloucity/settings.py
+++ falloucity/settings.py
@@ -27,7 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
-     'django.contrib.sitemaps',
+    'django.contrib.sitemaps',
 ]
 
 # ✅ middleware
